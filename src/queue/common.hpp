9015d882a6fbfd2885dd108c2dc3f89f8ac23828
Kean H Lau <khklau@users.noreply.github.com>
2016-01-05 00:27:21 +1100

    adding in new common header

---
+++ src/queue/common.hpp
@@ -0,0 +1,38 @@
+#ifndef BEAM_QUEUE_COMMON_HPP
+#define BEAM_QUEUE_COMMON_HPP
+
+#include <functional>
+
+namespace beam {
+namespace queue {
+namespace common {
+
+typedef uint16_t port;
+
+struct endpoint
+{
+    beam::internet::ipv4::address address;
+    beam::queue::common::port port;
+};
+
+} // namespace common
+} // namespace queue
+} // namespace beam
+
+namespace std {
+
+template<>
+struct hash<beam::queue::common::endpoint>
+{
+    std::size_t operator()(beam::queue::common::endpoint value) const
+    {
+	uint64_t tmp = value.address;
+	tmp = (tmp << 32) + value.port;
+	std::hash<uint64_t> hasher;
+	return hasher(tmp);
+    }
+};
+
+} // namespace std
+
+#endif
