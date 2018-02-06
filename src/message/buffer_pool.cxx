b16590ad3bacd0d1a8b873f9fbf6892e71689481
Kean H Lau <khklau@users.noreply.github.com>
2018-02-06 23:02:29 +1100

    initial implementation of the message buffer pool

---
+++ src/message/buffer_pool.cxx
@@ -0,0 +1,68 @@
+#include "buffer_pool.hpp"
+#include <utility>
+#include <turbo/container/mpmc_ring_queue.hxx>
+
+namespace {
+
+} // anonymous namespace
+
+namespace beam {
+namespace message {
+
+buffer_pool::buffer_pool(std::size_t message_size, capacity_type capacity)
+    :
+	pool_(),
+	free_list_(capacity)
+{
+    for (auto iter = pool_.begin(); iter != pool_.end(); ++iter)
+    {
+	*iter = std::move(kj::heapArray<capnp::word>(message_size));
+	free_list_.try_enqueue_copy(pool_.begin() - iter);
+    }
+}
+
+unique_pool_ptr buffer_pool::borrow()
+{
+    namespace tar = turbo::algorithm::recovery;
+    capacity_type reservation = 0U;
+    unique_pool_ptr result(nullptr, std::bind(&buffer_pool::release, this, std::placeholders::_1));
+    tar::retry_with_random_backoff([&] () -> tar::try_state
+    {
+	switch (free_list_.try_dequeue_copy(reservation))
+	{
+	    case free_list_type::consumer::result::success:
+	    {
+		result.reset(&(pool_[reservation]));
+		return tar::try_state::done;
+	    }
+	    default:
+	    {
+		return tar::try_state::retry;
+	    }
+	}
+    });
+    return std::move(result);
+}
+
+void buffer_pool::release(buffer* ptr)
+{
+    namespace tar = turbo::algorithm::recovery;
+    capacity_type index = &(pool_[0]) - ptr;
+    tar::retry_with_random_backoff([&] () -> tar::try_state
+    {
+	switch (free_list_.try_enqueue_copy(index))
+	{
+	    case free_list_type::producer::result::success:
+	    {
+		return tar::try_state::done;
+	    }
+	    default:
+	    {
+		return tar::try_state::retry;
+	    }
+	}
+    });
+}
+
+} // namespace message
+} // namespace beam
---
+++ src/message/buffer_pool.hpp
@@ -0,0 +1,41 @@
+#ifndef BEAM_MESSAGE_BUFFER_POOL_HPP
+#define BEAM_MESSAGE_BUFFER_POOL_HPP
+
+#include <cstdint>
+#include <functional>
+#include <tuple>
+#include <vector>
+#include <capnp/common.h>
+#include <kj/array.h>
+#include <turbo/container/mpmc_ring_queue.hpp>
+#include <turbo/toolset/attribute.hpp>
+
+namespace beam {
+namespace message {
+
+typedef kj::Array<capnp::word> buffer;
+
+typedef std::unique_ptr<buffer, std::function<void (buffer*)>> unique_pool_ptr;
+
+class TURBO_SYMBOL_DECL buffer_pool
+{
+public:
+    typedef std::uint32_t capacity_type;
+    buffer_pool(std::size_t message_size, capacity_type capacity);
+    unique_pool_ptr borrow();
+private:
+    typedef turbo::container::mpmc_ring_queue<capacity_type> free_list_type;
+    buffer_pool() = delete;
+    buffer_pool(const buffer_pool&) = delete;
+    buffer_pool(buffer_pool&&) = delete;
+    buffer_pool& operator=(const buffer_pool&) = delete;
+    buffer_pool& operator=(buffer_pool&&) = delete;
+    void release(buffer* ptr);
+    std::vector<buffer> pool_;
+    free_list_type free_list_;
+};
+
+} // namespace message
+} // namespace beam
+
+#endif
